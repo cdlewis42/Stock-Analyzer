5c7725af6e5303f4714f28f9b0cf7872c99066ff
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-02-10 19:22:35 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     var firebaseConfig = {
-            apiKey: "AIzaSyAqrxz9cVwxBcDv7lxISU2BGXI7fFj0yMg",
-            authDomain: "stockanalyser-73716.firebaseapp.com",
-            databaseURL: "https://stockanalyser-73716.firebaseio.com",
-            projectId: "stockanalyser-73716",
-            storageBucket: "stockanalyser-73716.appspot.com",
-            messagingSenderId: "243750173140",
-            appId: "1:243750173140:web:ee97836b0fe030ee257e41"
-          };  
+        apiKey: "AIzaSyAqrxz9cVwxBcDv7lxISU2BGXI7fFj0yMg",
+        authDomain: "stockanalyser-73716.firebaseapp.com",
+        databaseURL: "https://stockanalyser-73716.firebaseio.com",
+        projectId: "stockanalyser-73716",
+        storageBucket: "stockanalyser-73716.appspot.com",
+        messagingSenderId: "243750173140",
+        appId: "1:243750173140:web:ee97836b0fe030ee257e41"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
 
 });
